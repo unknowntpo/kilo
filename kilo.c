04cf545edc31e441a726b3f39820d8c88f9c14b1
unknowntpo <e850506@gmail.com>
2020-07-12 07:53:36 +0800

    Step 5: Turn off echoing

--- kilo.c
+++ kilo.c
@@ -36,10 +36,24 @@
  * Following the tutorial from https://viewsourcecode.org/snaptoken/kilo
  * and build from scratch
  */
+#include <termios.h>
 #include <unistd.h>
+
+void enableRawMode()
+{
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
+    enableRawMode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
