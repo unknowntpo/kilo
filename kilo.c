cde1ab7a4bd9a44b4b70f1a34606d4350ec691f0
unknowntpo <e850506@gmail.com>
2020-07-12 14:24:55 +0800

    Step 6: Disable raw mode at exit
    Let’s be nice to the user and restore their terminal’s
    original attributes when our program exits.
    We’ll save a copy of the termios struct in its original state,
    and use tcsetattr() to apply it to the terminal
    when the program exits.

--- kilo.c
+++ kilo.c
@@ -39,12 +39,19 @@
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode()
 {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
 
-    tcgetattr(STDIN_FILENO, &raw);
-
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
